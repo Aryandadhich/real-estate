7db0e681b0a921d64a2854d4fb8eccb80ccf3df2
aryandadhich <rajkumardadheech6@gmail.com>
2024-10-07 11:24:32 +0530

    navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,23 @@
+import "./navbar.scss"
+
 function Navbar (){
     return(
       <nav>
-        <div className="left">left</div>
-        <div className="right">right</div>
+        <div className="left">
+          <a href="/">
+          <img src="logo.png" alt="" />
+          <span>RajEstate</span>
+          </a>
+          <a href="/">Home</a>
+          <a href="/">About</a>
+          <a href="/">Contact</a>
+          <a href="/">Agents</a>
+        </div>
+        <div className="right">
+        <a href="/">Sign in</a>
+        <a href="/">Sign up</a>
+        </div>
+        
       </nav>
     )
 }
