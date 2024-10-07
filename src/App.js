0cce1b9720786831608339944b477225c3eca310
aryandadhich <rajkumardadheech6@gmail.com>
2024-10-07 10:41:41 +0530

    navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import "./Design/layout.scss"
+import Navbar  from './components/Navbar/Navbar'
 
 function App() {
   return (
     <div className='layout'>
-      Hello
+      <Navbar/>
     </div>
   );
 }
