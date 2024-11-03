1e45fbd673e1d6fa9603fc2e9ad674ffcfc5f271
aryandadhich <rajkumardadheech6@gmail.com>
2024-11-03 18:24:56 +0530

    homepage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import "./Design/layout.scss"
 import Navbar  from './components/Navbar/Navbar'
+import HomePage from './routes/HomePage/HomePage';
 
 function App() {
   return (
     <div className='layout'>
       <Navbar/>
+      <HomePage/>
     </div>
   );
 }
