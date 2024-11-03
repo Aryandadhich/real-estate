30c24de3f23ae478b59e77ff1dfecebb7fb8f743
aryandadhich <rajkumardadheech6@gmail.com>
2024-11-03 23:09:33 +0530

    Searchbar

--- src/components/searchBar/SearchBar.jsx
+++ src/components/searchBar/SearchBar.jsx
@@ -3,8 +3,21 @@ import React from 'react'
 
 function SearchBar() {
   return (
-    <div className='searchBar'> 
-      SearchBar
+    <div className='searchBar'>
+      <div className="type"></div>
+      <button>Buy</button>
+      <button>Rent</button>
+      <form>
+        <input type='text' name='location' placeholder='City Location'
+        />
+        <input type='number' name='minPrice' min={0} max={100000000} placeholder='Min Price'
+        />
+        <input type='number' name='maxPrice' min={0} max={100000000} placeholder='Max Price'
+        />
+        <button>
+          <img src='/search.png' alt='' />
+        </button>
+      </form>
     </div>
   )
 }
