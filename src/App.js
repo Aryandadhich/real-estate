3444ee6e0b41964390a657c3e81793bfe9e1d10a
aryandadhich <rajkumardadheech6@gmail.com>
2024-11-10 17:55:53 +0530

    all ok

--- src/App.js
+++ src/App.js
@@ -1,21 +1,34 @@
-import './App.css';
-import "./Design/layout.scss"
-import Navbar  from './components/Navbar/Navbar'
 import HomePage from './routes/HomePage/HomePage';
+import "./Design/layout.scss";
+import Navbar from './components/Navbar/Navbar';
+import { BrowserRouter, createBrowserRouter, RouterProvider } from 'react-router-dom';
+import ListPage from './routes/listPage/listPage';
 
 function App() {
-  return (
-    <div className='layout'>
-      <div className='navbar'>
-        
-      <Navbar/>
-      </div>
-      <div className='content'>
 
-      <HomePage/>
-      </div>
-      
-    </div>
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: (
+        <HomePage />
+      ),
+    },
+    {
+      path: "/list",
+      element: <ListPage />
+    }
+  ])
+  return (
+
+    // <div className="layout">
+    //   <div className="navbar">
+    //     <Navbar />
+    //   </div>
+    //   <div className='content'>
+    //     <HomePage />
+    //   </div>
+    // </div>
+    <RouterProvider router={router} />
   );
 }
 
---
+++ src/routes/listPage/listPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ListPage() {
+    return (
+        <div className='listPage'>
+            ListPage
+        </div>
+    )
+}
+
+export default ListPage;
