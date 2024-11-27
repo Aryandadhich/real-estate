8a80698a83c53560ec7b648acecafad2df237e3a
aryandadhich <rajkumardadheech6@gmail.com>
2024-11-27 23:18:27 +0530

    map

--- src/components/map/Map.jsx
+++ src/components/map/Map.jsx
@@ -1,17 +1,27 @@
-import { MapContainer, TileLayer } from "react-leaflet";
+import { MapContainer, TileLayer, useMap } from "react-leaflet";
 import "./map.scss";
 import "leaflet/dist/leaflet.css";
 import Pin from "../pin/Pin";
+import React from "react";
+
+function FitBounds({ items }) {
+  const map = useMap();
+
+  React.useEffect(() => {
+    const bounds = items.map(item => [item.latitude, item.longitude]);
+    if (bounds.length) {
+      map.fitBounds(bounds); // Automatically adjust the view to fit all pins
+    }
+  }, [items, map]);
+
+  return null;
+}
 
 function Map({ items }) {
   return (
     <MapContainer
-      center={
-        items.length === 1
-          ? [items[0].latitude, items[0].longitude]
-          : [52.4797, -1.90269]
-      }
-      zoom={7}
+      center={[20.5937, 78.9629]} // Geographic center of India
+      zoom={5} // Default zoom
       scrollWheelZoom={false}
       className="map"
     >
@@ -19,9 +29,10 @@ function Map({ items }) {
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      {items.map((item) => {
-        return <Pin item={item} key={item.id} />
-      })}
+      <FitBounds items={items} />
+      {items.map((item) => (
+        <Pin item={item} key={item.id} />
+      ))}
     </MapContainer>
   );
 }
--- src/components/pin/Pin.jsx
+++ src/components/pin/Pin.jsx
@@ -2,17 +2,26 @@ import React from 'react';
 import './pin.scss';
 import { Marker, Popup } from 'react-leaflet';
 import { Link } from "react-router-dom";
+import L from "leaflet";
+
+const customIcon = L.icon({
+    iconUrl: "/pin.png",  // Path to your custom icon image
+    iconSize: [25, 41],   // Specifies the size of the icon (width x height in pixels)
+    iconAnchor: [12, 41], // Specifies the anchor point of the icon (relative to the top-left corner)
+    popupAnchor: [1, -34] // Specifies where the popup appears relative to the marker
+});
+
 
 function Pin({ item }) {
     return (
-        <Marker position={[item.latitude, item.longitude]}>
+        <Marker position={[item.latitude, item.longitude]} icon={customIcon}>
             <Popup>
-                <div className="popupCotainer">
+                <div className="popupContainer">
                     <img src={item.images[0]} alt="" />
                     <div className="textContainer">
                         <Link to={`/${item.id}`}>{item.title}</Link>
                         <span>{item.bedroom} bedroom</span>
-                        <b>$ {item.price}</b>
+                        <b>₹ {item.price}</b>
                     </div>
                 </div>
             </Popup>
