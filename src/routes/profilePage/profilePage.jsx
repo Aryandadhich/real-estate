d4b2ad896ba0ee348de35d642ea181eece7977ea
aryandadhich <rajkumardadheech6@gmail.com>
2024-12-25 01:20:32 +0530

    profilepage

---
+++ src/routes/profilePage/profilePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./profilePage.scss";
+
+function ProfilePage() {
+    return (
+        <div className='profilePage'>
+            ProfilePage
+        </div>
+    )
+}
+
+export default ProfilePage
